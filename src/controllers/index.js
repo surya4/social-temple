cbb88a7edc40f0d42a1cfd8bb054538b1d4faadd
Surya Singh <me@suryasingh.xyz>
2017-10-13 01:02:34 +0530

    all apis done

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,7 +3,7 @@ const geocode_api_key = "AIzaSyCnKaZ7Q8zKqlAC4TBZz64iua1EYd8xlgc";
 const google = require('googleapis');
 var request = require("request")
 const place_api_key = "AIzaSyBjLuTq1WL8jvIQ5wb-qIWe712MxDmyu7A";
-
+var x;
 
 /* GET home page. */
 exports.index_get = (req, res) => {
@@ -16,7 +16,15 @@ exports.index_post = (req, res) => {
 };
 
 exports.temple_get = (req, res) => {
-    let name, image_link, address_link, nearby_link = [];
+    let name,
+        image_link,
+        address_link,
+        nearby_link = [];
+
+
+    console.log("caling global value -> " + x);
+
+    console.log("Calling from get -- > " + image_link);
     let location_name = req.params.temple_name;
     console.log("location name --> " + location_name);
     let location_unique_id;
@@ -47,10 +55,8 @@ function get_location_id(location_name) {
         url: geocode_url,
         json: true
     }, function(error, response, body) {
-        if (!error && response.statusCode === 200) {
+        if (!error && response.statusCode === 200 && body.status === "OK") {
             // console.log(body); // Print the json response
-            // console.log(body.results[0].place_id)
-            // body.results[0].place_id;
             get_location_details(body.results[0].place_id);
         }
     });
@@ -65,32 +71,87 @@ function get_location_details(location_id) {
         url: place_url,
         json: true
     }, function(error, response, body) {
-        if (!error && response.statusCode === 200) {
-            // console.log("Address -- >" +
-            //     body.result);
-            // getImage(body.result.formatted_address);
-            // getAddress(body.result.icon);
+        console.log("received ---- " + response.statusCode);
+        if (!error && response.statusCode === 200 && body.status === "OK") {
+            var result = body.result;
+            if (result == undefined) {
+                console.log("got undefines - " + JSON.stringify(body));
+            } else {
+                var out_result = result;
+                if (out_result == undefined) {
+                    console.log("undefined addr - " + JSON.stringify(out_result));
+                } else {
+                    console.log(" value - " + JSON.stringify(out_result.name));
+                    x = out_result;
+                    // getTempleDetails(out_result);
+                    getName(JSON.stringify(out_result.name));
+                    getImage(JSON.stringify(out_result.formatted_address));
+                    getAddress(JSON.stringify(out_result.icon));
+                    getNearByLocations(out_result.geometry.location.lat, out_result.geometry.location.lng, out_result.types[0]);
+
+                }
+
+            }
         }
 
     });
 };
 
-function getName(name_link) {
-    return 'Rajendra Sarovar';
-    // return name_link;
+
+
+function getName(name) {
+    // var temp_name = name;
+    console.log("Geeting name " + name);
+    // return 'Rajendra Sarovar';
+    return name;
 }
 
 function getImage(image_link) {
-    return 'https://maps.gstatic.com/mapfiles/place_api/icons/worship_hindu-71.png';
-    // return image_link;
+    // return 'https://maps.gstatic.com/mapfiles/place_api/icons/worship_hindu-71.png';
+    return image_link;
 }
 
 function getAddress(address_link) {
-    return 'Bihar, Sadhnapuri, Sadhapur, Chhapra, Bihar 841301, India';
-    // return address_link;
+    // return 'Bihar, Sadhnapuri, Sadhapur, Chhapra, Bihar 841301, India';
+    return address_link;
 }
 
-function getNearByTemples(nearby_link) {
-    var arr = ["वेंकितापुरम पेरूमल कोइल (SRI करानाराजर सनिधि)", "खाखी महाराज मंदिर", "अप्पा पिथियम स्वामी मंदिर", "शिव मंदिर", "नीलकंठ मंदिर", "इस्क्कों गुरुवायुर", "Swaminarayan mandir", "DADA MAI WALA MANDIR BANKNER", "हनुमान मंदिर", "हनुमान एंड श्री बालाजी मंदिर", "सिथाम्पलम सिवान मंदिर", "झूले लाल मंदिर", "विश्वकर्मा मंदिर", "काली मंदिर", "श्री सिद्धि विनायक गणेश मन्दिर"];
-    return arr;
+// function getNearByTemples(nearby_link) {
+//     var arr = ["वेंकितापुरम पेरूमल कोइल (SRI करानाराजर सनिधि)", "खाखी महाराज मंदिर", "अप्पा पिथियम स्वामी मंदिर", "शिव मंदिर", "नीलकंठ मंदिर", "इस्क्कों गुरुवायुर", "Swaminarayan mandir", "DADA MAI WALA MANDIR BANKNER", "हनुमान मंदिर", "हनुमान एंड श्री बालाजी मंदिर", "सिथाम्पलम सिवान मंदिर", "झूले लाल मंदिर", "विश्वकर्मा मंदिर", "काली मंदिर", "श्री सिद्धि विनायक गणेश मन्दिर"];
+//     return arr;
+// }
+
+function getNearByLocations(lat, lng, typ) {
+    console.log("lat -- >" + lat);
+    console.log("lng -- >" + lng);
+    console.log("typ -- >" + typ);
+    let location_array = [];
+    var near_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + lng + "&radius=5000&type=" + typ + "&key=" + place_api_key;
+    request({
+        url: near_url,
+        json: true
+    }, function(error, response, body) {
+        console.log("received ---- nearby " + response.statusCode);
+        if (!error && response.statusCode === 200 && body.status === "OK") {
+            var result = body.results;
+            if (result == undefined) {
+                console.log("got undefines - " + JSON.stringify(body));
+            } else {
+                var out_result = result;
+                if (out_result == undefined) {
+                    console.log("undefined addr - " + JSON.stringify(out_result));
+                } else {
+                    out_result.forEach(function(i) {
+                        location_array.push(i.name);
+                        console.log(location_array);
+                    }, this);
+
+
+
+                }
+
+            }
+        }
+
+    });
 }
