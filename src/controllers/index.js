1649b0130628fe311ee94b9e9bd133c2332940a0
Surya Singh <me@suryasingh.xyz>
2017-10-13 09:44:23 +0530

    removing extra comments

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,32 +1,10 @@
 const express = require('express');
-const geocode_api_key = "AIzaSyCnKaZ7Q8zKqlAC4TBZz64iua1EYd8xlgc";
 const google = require('googleapis');
-var request = require("request")
+const request = require("request");
+
+// google api secrets
+const geocode_api_key = "AIzaSyCnKaZ7Q8zKqlAC4TBZz64iua1EYd8xlgc";
 const place_api_key = "AIzaSyBjLuTq1WL8jvIQ5wb-qIWe712MxDmyu7A";
-var x;
-
-// function getName(name) {
-//     // var temp_name = name;
-//     console.log("Geeting name " + name);
-//     // return 'Rajendra Sarovar';
-//     return name;
-// }
-
-// function getImage(image_link) {
-//     // return 'https://maps.gstatic.com/mapfiles/place_api/icons/worship_hindu-71.png';
-//     return image_link;
-// }
-
-// function getAddress(address_link) {
-//     // return 'Bihar, Sadhnapuri, Sadhapur, Chhapra, Bihar 841301, India';
-//     return address_link;
-// }
-
-// function getNearByTemples(array_link) {
-//     // var arr = ["वेंकितापुरम पेरूमल कोइल (SRI करानाराजर सनिधि)", "खाखी महाराज मंदिर", "अप्पा पिथियम स्वामी मंदिर", "शिव मंदिर", "नीलकंठ मंदिर", "इस्क्कों गुरुवायुर", "Swaminarayan mandir", "DADA MAI WALA MANDIR BANKNER", "हनुमान मंदिर", "हनुमान एंड श्री बालाजी मंदिर", "सिथाम्पलम सिवान मंदिर", "झूले लाल मंदिर", "विश्वकर्मा मंदिर", "काली मंदिर", "श्री सिद्धि विनायक गणेश मन्दिर"];
-//     console.log(array_link);
-//     return array_link;
-// }
 
 /* GET home page. */
 exports.index_get = (req, res) => {
@@ -37,29 +15,24 @@ exports.index_post = (req, res) => {
     res.render('pages/index');
 };
 exports.temple_get = (req, res) => {
-    var meta_data = [];
+    var all_data = [];
     var nearby_list = [];
-    console.log("caling global value -> " + x);
-    // console.log("Calling from get -- > " + image_link);
+
+    // getting name of place from params
     let location_name = req.params.temple_name;
     console.log("location name --> " + location_name);
     let location_unique_id;
+
+    // using promise to implement apis one after another
     get_location_id(location_name).then(function(place_id) {
         console.log('bat', place_id)
-        get_location_details(place_id).then(function(meta_data) {
-            // console.log(meta_data);
-
-
-            getNearByLocations(meta_data).then(function(nearby_list) {
-
-                // nearby_list = meta_data[3].split(',');
-                // console.log(nearby_list);
-
-                // console.log("nearby_list -----> " + nearby_list);
-                console.log("Calling Image -- > " + meta_data);
+        get_location_details(place_id).then(function(all_data) {
+            console.log(all_data);
+            getNearByLocations(all_data).then(function(nearby_list) {
+                console.log("Calling Image -- > " + all_data);
                 res.header('Access-Control-Allow-Origin', "*");
                 res.render('pages/temple', {
-                    data: meta_data,
+                    data: all_data,
                     near_data: nearby_list
                 });
             }).catch((err) => {
@@ -69,6 +42,8 @@ exports.temple_get = (req, res) => {
     });
 };
 
+
+// implementing geocode api to get place id for a given search of string
 function get_location_id(location_name) {
     return new Promise(function(resolve) {
         let modified_name = location_name.split(' ').join('+');
@@ -80,14 +55,15 @@ function get_location_id(location_name) {
             json: true
         }, function(error, response, body) {
             if (!error && response.statusCode === 200 && body.status === "OK") {
-                // console.log(body); // Print the json response
-                // console.log(body, 'kehs');
+                // console.log("body"+body); // Print the json response
                 resolve(body.results[0].place_id);
             }
         });
     });
 };
 
+
+// implementing place api to get location name, address and icon from received location id from previous result
 function get_location_details(location_id) {
     return new Promise(function(resolve) {
         // get details of place using place id
@@ -108,7 +84,6 @@ function get_location_details(location_id) {
                         console.log("undefined addr - " + JSON.stringify(out_result));
                     } else {
                         console.log(" value - " + JSON.stringify(out_result.name));
-                        x = out_result;
                         // getTempleDetails(out_result);
                         resolve([out_result.name, out_result.formatted_address, out_result.icon, [JSON.stringify(out_result.geometry.location.lat), JSON.stringify(out_result.geometry.location.lng), JSON.stringify(out_result.types[0])]]);
                     }
@@ -118,10 +93,11 @@ function get_location_details(location_id) {
     });
 };
 
+
+// implementing place api with near by search to get nearby worship places under 5000 metre using latittude, longitude and types from result 
 function getNearByLocations(inp_arr) {
     return new Promise(function(resolve) {
         var lat_arr = JSON.parse("[" + inp_arr[3] + "]");
-        console.log(" check if input is array --- >" + lat_arr);
         console.log("lat -- >" + lat_arr[0]);
         console.log("lng -- >" + lat_arr[1]);
         console.log("typ -- >" + lat_arr[2]);
